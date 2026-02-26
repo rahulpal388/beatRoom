72d7bd456589006808d7192c6ad9962d430c884b
rahulpal388 <rahulschoolmail59@gmail.com>
2026-02-27 04:45:26 +0530

    env misconfig

--- frontend/src/api/baseUrlAxios.ts
+++ frontend/src/api/baseUrlAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/v1",
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     withCredentials: true
 })
 

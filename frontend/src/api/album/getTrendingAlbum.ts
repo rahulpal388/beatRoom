528ba9f83d7255f37677cf6622f636979214b19a
rahulpal388 <rahulschoolmail59@gmail.com>
2026-03-23 16:29:51 +0530

    fix bug

--- frontend/src/api/album/getTrendingAlbum.ts
+++ frontend/src/api/album/getTrendingAlbum.ts
@@ -4,5 +4,10 @@ import { AxiosInstance } from "axios";
 
 
 export async function getTrendingAlbum(api: AxiosInstance, limit: number, page: number, language: string): Promise<IAlbum[]> {
-    return (await api.get(`/album/trendingAlbum?limit=${limit}&page=${page}&language=${language}`)).data;
+    try {
+
+        return (await api.get(`/album/trendingAlbum?limit=${limit}&page=${page}&language=${language}`)).data;
+    } catch {
+        return []
+    }
 }

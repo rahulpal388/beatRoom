f8373d211e29e32ecc7d63d2c9d3b3b4752e7630
rahulpal388 <rahulschoolmail59@gmail.com>
2026-03-29 01:31:16 +0530

    fix

--- backend/src/websocket/broadCast.ts
+++ backend/src/websocket/broadCast.ts
@@ -5,8 +5,8 @@ import { allConnection } from "./listenConnectionFn.js";
 
 export function broadcast(roomId: string, senderId: string, message: string) {
 
-    const reciverClient = allConnection.get(roomId)?.members.filter(member => member.userId !== senderId) ?? [];
-    reciverClient.forEach(reciver => {
-        reciver.socket.send(message);
-    })
+    // const reciverClient = allConnection.get(roomId)?.members.(member => member.userId !== senderId) ?? [];
+    // reciverClient.forEach(reciver => {
+    //     reciver.socket.send(message);
+    // })
 }
